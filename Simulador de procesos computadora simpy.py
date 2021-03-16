81ac00fa709a3176d174c7b1e7ed5df20f42d181
birdpearl <77627803+birdpearl@users.noreply.github.com>
2021-03-16 07:35:55 -0600

    Se integra simulador y reforma (Entrega oficial)

---
+++ Simulador de procesos computadora simpy.py
@@ -0,0 +1,69 @@
+#Autores: Ana Ramírez y Diego Perdomo 
+#
+
+import simpy
+import random
+import math
+
+tiempoT = 0
+env = simpy.Environment() 
+RAM = simpy.Container(env, init=100 , capacity=100)# Capacidad de ram
+pro= simpy.Resource(env,capacity = 2)#cantidad de procesadores
+#acumulador del total de tiempo utilizado para los procesos
+Ttotal = 0
+#arreglo que almacena los diferentes tiempos para calcular la desviacion estandar 
+times = []
+def compu(env,inst,num,tiempo_llegada,RAM,pro):
+    global times
+    global Ttotal
+    random_ram = 0 
+    global tiempoT
+    instrN = 0 
+    yield env.timeout(tiempo_llegada)
+    llegada = env.now ###########
+    
+    if RAM.level >= random_ram:
+        random_ram = random.randint(1,10)
+    
+    tiempo_procesando = random_ram
+    
+    with RAM.get(random_ram) as turn:
+        yield turn      
+        yield env.timeout(tiempo_procesando) 
+        
+        instrN = inst - 3#intrucciones por entrada al procesador ########
+        with pro.request() as simular:#######
+            yield simular
+            if(instrN <= 0):
+                instrN = 0
+            yield env.timeout(1)
+            desicion = random.randint(1,2)
+            if(desicion == 2):
+                yield env.timeout(1)      
+        if instrN<3:
+            yield env.timeout(5)#intervalos de tiempo
+        RAM.put(random_ram)
+        
+    tiempo = env.now - llegada    
+    Ttotal = Ttotal + tiempo
+    times.append(tiempo)
+    print ('%s,%f' % (num, tiempo))
+        
+cantprocesos = 200 #cantidad de procesos
+for i in range(cantprocesos):
+    env.process(compu(env,random.randint(1,10),i,random.expovariate(1.0/1),RAM,pro))
+env.run()
+#calculo de la media
+media = Ttotal / cantprocesos  
+sumatoria = 0
+varianza = 0
+desvest = 0
+#ciclo para obtener la sumatoria de la fórmula de la desviación estándar
+for i in times:
+    temp = (i - media)
+    temp2 = math.pow(temp, 2)
+    sumatoria = sumatoria + temp2
+desvest = math.sqrt((sumatoria / cantprocesos))
+print("\n Media:",media)
+print("\n Desviación estándar ", desvest)
+        
